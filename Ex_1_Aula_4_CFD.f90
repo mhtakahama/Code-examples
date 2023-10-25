6bdc0a4fd3616622e658eaabedbd07e4d4354c26
Marcos Takahama <marcostakahama@alunos.utfpr.edu.br>
2023-10-25 07:59:45 -0300

    Update Ex_1_Aula_4_CFD.f90

--- Ex_1_Aula_4_CFD.f90
+++ Ex_1_Aula_4_CFD.f90
@@ -1,43 +1,42 @@
     !****************************************************************************
-    ! Doutorado em Engenharia Mecânica PPGEM
-    ! Universidade Tecnológica Federal do Paraná
+    ! Doutorado em Engenharia MecÃ¢nica PPGEM
+    ! Universidade TecnolÃ³gica Federal do ParanÃ¡
     ! Campus Curitiba
     ! Aluno: Marcos Takahama
-    ! Professor: Paulo Henrique Dias dos Santos
     ! Disciplina: CFD
     ! Setembro de 2020
     !****************************************************************************
-    ! Determine o perfil de temperatura de uma barra de aço (K=25 W/mK) de 1 metro
+    ! Determine o perfil de temperatura de uma barra de aÃ§o (K=25 W/mK) de 1 metro
     !de comprimento com temperaturas prescritas nas extremidades. Na extremidade em 
-    !x=0, a temperatura vale 150ºC e em x=L vale 50ºC
+    !x=0, a temperatura vale 150ÂºC e em x=L vale 50ÂºC
     !
     !Ex_1_Aula_4_CFD.f90
     !
     ! SUBROUTINES:
     ! jacobi
     !****************************************************************************
-    !1 - Nome do codigo e entrada de parâmetros
+    !1 - Nome do codigo e entrada de parÃ¢metros
     program Ex_1_Aula_4_CFD
 
-    !Declaração de variáveis; integer=integer*4 (4bytes); real=real*4
+    !DeclaraÃ§Ã£o de variÃ¡veis; integer=integer*4 (4bytes); real=real*4
     !parameter=valor constante
-    !dimension=declaração de vetor ou matriz
+    !dimension=declaraÃ§Ã£o de vetor ou matriz
 
-    implicit none !Variáveis implicitas
-    integer, parameter:: nv=5 !Número de Vomules de Controles
+    implicit none !VariÃ¡veis implicitas
+    integer, parameter:: nv=5 !NÃºmero de Vomules de Controles
     integer:: i, it_final
     real:: L, dx, T_est, erro, kmax, k, residue
     real, dimension(nv):: Aw, Ap, Ae, Su, Sp
     real, dimension(nv):: T, x
     real, dimension(nv,nv):: E
     real:: Tp1, Tp2
-    !Inputs de usuário
+    !Inputs de usuÃ¡rio
     write(*,*) 'Qual o valor da temperatura da parede em x=0'
     read(*,*) Tp1
     write(*,*) 'Qual o valor da temperatura da parede x=L'
     read(*,*) Tp2
     !****************************************************************************
-    !2 - Método VF
+    !2 - MÃ©todo VF
     L = 1.0
     dx = L/nv
     k = 25
@@ -45,22 +44,22 @@
     erro = 0.0001
     kmax = 8*10**5
 
-    !Estimativa do campo inicial da variável
+    !Estimativa do campo inicial da variÃ¡vel
     do i=1,nv !for
         T(i) = T_est
     end do
 
-    !Cálculo da malha:
+    !CÃ¡lculo da malha:
     x(1) = 0.5D0*dx !D0 = double precision
     do i=2,nv
         x(i) = x(i-1) + dx
     end do
 
     write(*,*) 'Posicoes da malha'
-    write(*,*) x !Escreve a posição da malha
+    write(*,*) x !Escreve a posiÃ§Ã£o da malha
     pause
 
-    !Cálculo temperatura através do MVF
+    !CÃ¡lculo temperatura atravÃ©s do MVF
     do i=1,nv
         if (i==1) then
             Ae(i)=1
@@ -98,14 +97,14 @@
     write(*,*) 'Vetor B'
     write(*,*) Su !Escreve o vetor B
     pause
-    !Subrotina para o cálculo dos coeficientes:
+    !Subrotina para o cÃ¡lculo dos coeficientes:
     call jacobi(Ae,Aw,Su,Ap,nv,T,erro,kmax,it_final,residue)
 
     write(*,*) 'erro calculado'
-    write(*,*) residue !Escreve o numero de iterações pelo método de jacobi
+    write(*,*) residue !Escreve o numero de iteraÃ§Ãµes pelo mÃ©todo de jacobi
 
     write(*,*) 'numero de iteracoes para convergencia'
-    write(*,*) it_final !Escreve o numero de iterações pelo método de jacobi
+    write(*,*) it_final !Escreve o numero de iteraÃ§Ãµes pelo mÃ©todo de jacobi
 
     write(*,*) 'Temperatura calculada nos pontos nodais'
     write(*,*) T !Escreve temperaturas calculados
@@ -115,7 +114,7 @@
     !3 - "Plot resultados"
 
     !Resultados plotados no TecPlot:
-    !Temperatura através de MVF:
+    !Temperatura atravÃ©s de MVF:
 150 format(2x,30(F20.12,1X))
     open(15,file="Tnum.plt")
     write (15,*) 'Title ="Tnum"'
@@ -128,11 +127,11 @@
 
     !****************************************************************************
     !4 - Subrotinas
-    Contains !As subrotinas são separadas do programa principal por esse comando
-    !Subrotina do Método Jacobi
+    Contains !As subrotinas sÃ£o separadas do programa principal por esse comando
+    !Subrotina do MÃ©todo Jacobi
     !   call jacobi(Ae,Aw,Su,Ap,nv,T,erro,kmax)
     !****************************************************************************
-    !Método de Jacobi
+    !MÃ©todo de Jacobi
     SUBROUTINE jacobi(Ae,Aw,Su,Ap,N,A,erro,kmax,it_final,residue)
     implicit none
     integer:: i,k,it_final
@@ -142,60 +141,60 @@
     real, dimension(1:N,1:2):: M
     real, dimension(N):: Ri, A
     real :: RMS, erro, kmax, residue
-    !it_final: armazena a última iteração convergido ou não
-    !M: matriz problema (posição x iteração)
-    !Ri: resíduo do ponto i
-    !RMS: resíduo médio quadrático da iteração k+1
-    !1) Estimativa inicial da variável
+    !it_final: armazena a Ãºltima iteraÃ§Ã£o convergido ou nÃ£o
+    !M: matriz problema (posiÃ§Ã£o x iteraÃ§Ã£o)
+    !Ri: resÃ­duo do ponto i
+    !RMS: resÃ­duo mÃ©dio quadrÃ¡tico da iteraÃ§Ã£o k+1
+    !1) Estimativa inicial da variÃ¡vel
     do i=1,N
-        M(i,1) = A(i) !A(i) é oriunda do programa principal
+        M(i,1) = A(i) !A(i) Ã© oriunda do programa principal
     enddo
     RMS=0
-    !2) Início do processo iterativo
-    k=1 !Inicialização do contador de iterações
+    !2) InÃ­cio do processo iterativo
+    k=1 !InicializaÃ§Ã£o do contador de iteraÃ§Ãµes
     do while (k<kmax)
-        !Cálculo da variável com os valores da iteração anterior
+        !CÃ¡lculo da variÃ¡vel com os valores da iteraÃ§Ã£o anterior
         do i=1,N
             if(i==1) then !Primeiro Volume de Controle
                 M(i,2)=(Ae(i)*M(i+1,1)+Su(i))/Ap(i)
             elseif((i>1).and.(i<N)) then !Volumes de Controle Internos
                 M(i,2)=(Aw(i)*M(i-1,1)+Ae(i)*M(i+1,1)+Su(i))/Ap(i)
-            else !Último Volume de Controle
+            else !Ãšltimo Volume de Controle
                 M(i,2)=(Aw(i)*M(i-1,1)+Su(i))/Ap(i)
             endif
         enddo
 
-        !3) Cálculo do Resíduo
+        !3) CÃ¡lculo do ResÃ­duo
         do i=1,N
             if((i>1).and.(i<N)) then
-                !Resíduo Local
+                !ResÃ­duo Local
                 Ri(i)=abs(Su(i)+Ae(i)*M(i+1,2)+Aw(i)*M(i-1,2)-Ap(i)*M(i,2))
                 !write(*,*) i,  Ri(i)
                 !pause
-                ! Resíduo da Iteração
-                RMS=RMS+Ri(i)**2.0D0  !usa o residuo maximo calculado em cada nó
+                ! ResÃ­duo da IteraÃ§Ã£o
+                RMS=RMS+Ri(i)**2.0D0  !usa o residuo maximo calculado em cada nÃ³
             endif
         enddo
         RMS = RMS**0.5D0
 
-        !4) Avanço da iteração
+        !4) AvanÃ§o da iteraÃ§Ã£o
         do i=1,N
             M(i,1)=M(i,2)
         enddo
 
-        !5) Verificação da convergência
-        if(RMS<erro+1) then !CONVERGÊNCIA: RMS tende a 1
-            it_final = k !armazena a última posição iterativa
-            k=kmax !força o fim da iterações
+        !5) VerificaÃ§Ã£o da convergÃªncia
+        if(RMS<erro+1) then !CONVERGÃŠNCIA: RMS tende a 1
+            it_final = k !armazena a Ãºltima posiÃ§Ã£o iterativa
+            k=kmax !forÃ§a o fim da iteraÃ§Ãµes
             residue = RMS-1
-        else !SEM CONVERGÊNCIA:
+        else !SEM CONVERGÃŠNCIA:
             k=k+1 !incremento de k
-            it_final = k !armazena a última iteração ->kmax
+            it_final = k !armazena a Ãºltima iteraÃ§Ã£o ->kmax
             residue = RMS-1
             !write(*,*) k, RMS
             !pause
         endif
-    enddo !Finalização do contador de iterações
+    enddo !FinalizaÃ§Ã£o do contador de iteraÃ§Ãµes
 
     !6) Resultado que retorna para o programa principal
     do i=1,N
